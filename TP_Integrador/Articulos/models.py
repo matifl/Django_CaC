212c5cc63615ddb2183ec08baade8c5315c7a8c5
Cecilia Ramos <cheberamos@gmail.com>
2022-10-24 12:42:52 -0300

    Corrijo errores de presentacion de los modelos dentro del admin

--- TP_Integrador/Articulos/models.py
+++ TP_Integrador/Articulos/models.py
@@ -12,21 +12,27 @@ class Articulo(models.Model):
     imagen_url=models.ImageField(verbose_name="Imagen",upload_to="articulos")
     # created=models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     # updated=models.DateTimeField(auto_now=True, verbose_name="Fecha de modificación")
-
-class Categoria(models.Model):
-    id_categoria=models.CharField(primary_key=True,max_length=2,verbose_name="Id_Categoría")
-    desc_categoria=models.TextField(max_length=15,verbose_name="Descripción Categoría")
-
-class Subcategoria(models.Model):
-    id_subcategoria=models.TextField(primary_key=True,max_length=10,verbose_name="Descripción Subcategoría")
-    id_categoria=models.CharField(max_length=2,verbose_name="Id_Categoría")
-    desc_subcategoria=models.TextField(max_length=40,verbose_name="Descripción Subcategoría")
-
-
     def __str__(self):
-        return self.title
-    
+        return self.nombre
+        
     class Meta:
         verbose_name ="articulo"
         verbose_name_plural="articulos"
         # ordering= ["id"]
+
+    
+class Categoria(models.Model):
+    id_categoria=models.CharField(primary_key=True,max_length=2,verbose_name="Id_Categoría")
+    desc_categoria=models.TextField(max_length=15,verbose_name="Descripción Categoría")
+
+    def __str__(self):
+        return self.desc_categoria
+
+class Subcategoria(models.Model):
+    id_subcategoria=models.TextField(primary_key=True,max_length=10,verbose_name="Id Subcategoría")
+    id_categoria=models.CharField(max_length=2,verbose_name="Id_Categoría")
+    desc_subcategoria=models.TextField(max_length=40,verbose_name="Descripción Subcategoría")
+
+    def __str__(self):
+        return self.desc_subcategoria
+
