01617f90ff1aca1a3fc7e757a91dcda321616ac7
Matias <flm40853@gmail.com>
2022-11-01 20:04:36 -0300

    Modifico contact form

--- TP_Integrador/MundoKid/forms.py
+++ TP_Integrador/MundoKid/forms.py
@@ -2,7 +2,7 @@ from cProfile import label
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Nombre', max_length=100)
-    surname = forms.CharField(label='Apellido', max_length=100)
-    mail = forms.EmailField(label='Email', max_length=100)
-    comment = forms.CharField(label='Deja tu comentario!',widget=forms.Textarea)
+    name = forms.CharField(label='Nombre', max_length=100,required=True)
+    surname = forms.CharField(label='Apellido', max_length=100,required=True)
+    mail = forms.EmailField(label='Email', max_length=100,required=True)
+    comment = forms.CharField(label='Deja tu comentario!',widget=forms.Textarea,required=False)
